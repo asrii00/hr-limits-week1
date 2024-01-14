7fd3126c482f0876484cc5e5c190fe7d8bdc5d23
asrii00 <t2rias00@students.oamk.fi>
2024-01-14 11:47:19 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  function calculate(){
+
+
+    const lower = (220- (age.replace(',','.'))) * 0.65
+    const upper = (220- (age.replace(',','.'))) * 0.85
+    setLower(lower)
+    setUpper(upper)
+
+  }
+
+  const [lower, setLower] = useState('');
+  const [upper, setUpper] = useState('');
+  const [age, setAge] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}> 
+      {"\n"}Age</Text>
+      <TextInput keyboardType='decimal-pad' style={styles.field} value={age} onChangeText={text => setAge(text)}/>
+      <Text style={styles.text}>Hr limits</Text>
+      <Text style={styles.text}>{(Number(lower)).toFixed(0)} - {(Number(upper)).toFixed(0)}</Text>
+      <Button title="Calculate" onPress={calculate}></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +34,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
+  field: {
+    backgroundColor: '#fff', 
+    fontSize: 18 
+  }, 
+  text:{
+    fontSize: 18 
+  }
 });
